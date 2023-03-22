3a65aa3e8cbd92e3991e9715ae6836cc6bbf75e4
SedoyLuka <lukasedoy@gmail.com>
2023-03-22 05:44:28 +0300

    подготовка проекта для сдачи

---
+++ source/js/accordion.js
@@ -0,0 +1,4 @@
+const accordionParts = document.querySelectorAll('.footer__button-wrapper');
+const acoordionContent = document.querySelectorAll('.footer__accordion-content');
+const breakpoint = window.matchMedia('(min-width:768px)');
+const accordionButtons = document.querySelectorAll('.footer__section-title');
