13a8dc200f468bb481d77faf01bb8f2a8b6580b8
SedoyLuka <lukasedoy@gmail.com>
2023-04-18 01:02:48 +0300

    правки формы и исправление аккордиона

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,49 +1,118 @@
-const accordionParts = document.querySelectorAll('.footer__button-wrapper');
-const acoordionContent = document.querySelectorAll('.footer__accordion-content');
-const breakpoint = window.matchMedia('(min-width:768px)');
-const accordionButtons = document.querySelectorAll('.footer__section-title');
+// const accordionParts = document.querySelectorAll('.footer__button-wrapper');
+// const acoordionContent = document.querySelectorAll('.footer__accordion-content');
+// const breakpoint = window.matchMedia('(min-width:768px)');
+// const accordionButtons = document.querySelectorAll('.footer__section-title');
 
+
+// const hideAccordionContent = (action) => {
+//   acoordionContent.forEach((el) => {
+//     el.classList[action]('visually-hidden');
+//   });
+//   accordionButtons.forEach((el) => {
+//     el.style.backgroundImage = 'url("./img/svg/plus.svg")';
+//   });
+// };
+
+// const breakpointChecker = () => {
+//   if (!breakpoint.matches) {
+//     if (accordionParts) {
+//       hideAccordionContent('add');
+//       for (let i = 0; i < accordionParts.length; i++) {
+//         accordionParts[i].addEventListener('click', (evt) => {
+//           let content = evt.currentTarget.closest('section').querySelector('.footer__accordion-content');
+//           let button = evt.currentTarget.closest('section').querySelector('.footer__section-title');
+//           if (accordionParts[i].getAttribute('data-accordion-state') === 'close') {
+//             hideAccordionContent('add');
+//             content.classList.remove('visually-hidden');
+//             accordionParts[i].setAttribute('data-accordion-state', 'open');
+//             button.style.backgroundImage = 'url("./img/svg/minus.svg")';
+//             content.scrollIntoView({
+//               behavior: 'smooth',
+//               block: 'start',
+//             });
+//           } else {
+//             hideAccordionContent('add');
+//             accordionParts[i].setAttribute('data-accordion-state', 'close');
+//           }
+//         });
+//       }
+//     }
+//   } else {
+//     hideAccordionContent('remove');
+//     accordionButtons.forEach((el) => {
+//       el.style.backgroundImage = 'none';
+//     });
+//   }
+// };
+
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+const accordionParts = document.querySelectorAll(".footer__button-wrapper");
+const acoordionContent = document.querySelectorAll(
+  ".footer__accordion-content"
+);
+const breakpoint = window.matchMedia("(min-width:768px)");
+const accordionButtons = document.querySelectorAll(".footer__section-title");
 
 const hideAccordionContent = (action) => {
   acoordionContent.forEach((el) => {
-    el.classList[action]('visually-hidden');
+    el.classList[action]("visually-hidden");
   });
   accordionButtons.forEach((el) => {
     el.style.backgroundImage = 'url("./img/svg/plus.svg")';
   });
 };
 
+const onPartClick = (evt) => {
+  let content = evt.currentTarget
+    .closest("section")
+    .querySelector(".footer__accordion-content");
+  let button = evt.currentTarget
+    .closest("section")
+    .querySelector(".footer__section-title");
+    for (let i = 0; i < accordionParts.length; i++) {
+
+  if (accordionParts[i].getAttribute("data-accordion-state") === "close") {
+    hideAccordionContent("add");
+    content.classList.remove("visually-hidden");
+    accordionParts[i].setAttribute("data-accordion-state", "open");
+    button.style.backgroundImage = 'url("./img/svg/minus.svg")';
+    content.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  } else {
+    hideAccordionContent("add");
+    accordionParts[i].setAttribute("data-accordion-state", "close");
+  }
+}
+};
+
 const breakpointChecker = () => {
   if (!breakpoint.matches) {
-    if (accordionParts) {
-      hideAccordionContent('add');
-      for (let i = 0; i < accordionParts.length; i++) {
-        accordionParts[i].addEventListener('click', (evt) => {
-          let content = evt.currentTarget.closest('section').querySelector('.footer__accordion-content');
-          let button = evt.currentTarget.closest('section').querySelector('.footer__section-title');
-          if (accordionParts[i].getAttribute('data-accordion-state') === 'close') {
-            hideAccordionContent('add');
-            content.classList.remove('visually-hidden');
-            accordionParts[i].setAttribute('data-accordion-state', 'open');
-            button.style.backgroundImage = 'url("./img/svg/minus.svg")';
-            content.scrollIntoView({
-              behavior: 'smooth',
-              block: 'start',
-            });
-          } else {
-            hideAccordionContent('add');
-            accordionParts[i].setAttribute('data-accordion-state', 'close');
-          }
-        });
-      }
-    }
+    hideAccordionContent("add");
+    accordionParts.forEach((el) => {
+      el.addEventListener("click", onPartClick);
+    });
   } else {
-    hideAccordionContent('remove');
+    hideAccordionContent("remove");
+    accordionParts.forEach((el) => {
+      el.removeEventListener("click", onPartClick);
+    });
     accordionButtons.forEach((el) => {
-      el.style.backgroundImage = 'none';
+      el.style.backgroundImage = "none";
     });
   }
 };
 
-breakpoint.addListener(breakpointChecker);
-breakpointChecker();
+const init = () => {
+  if (!accordionParts) {
+    return;
+  }
+
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
+};
+
+init();
